a0c8e01c7c0876b99acf2cb088ea2cde518feaf2
1836.person <julian.cheukyin.chan@gmail.com>
2025-04-19 13:35:45 +0800

    Add battery, fix aside (?) and add clock (?)

--- General assets/scripts.js
+++ General assets/scripts.js
@@ -3,5 +3,5 @@ function updateGreet() {
     const now = new Date();
     const hours = now.getHours().toString().padStart(2, "0");
     const minutes = now.getMinutes().toString().padStart(2, "0");
-    clockElement.textContent = `${hours}:${minutes}:${seconds}`;
+    clockElement.textContent = `${hours}:${minutes}`;
 }
